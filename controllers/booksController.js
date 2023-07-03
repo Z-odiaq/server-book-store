c659bb0c67cde8e52f98c6597b9cf3ebfaf633a6
Mohamed Ammar <mohamedammar10@live.fr>
2023-07-03 16:21:52 +0100

    Added Cloudinary,favorites, search, filters

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,32 +1,95 @@
 const Book = require('../models/Book');
 const User = require('../models/User');
-//import objectId
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+
 const mongoose = require("mongoose");
 
-exports.createBook = (req, res) => {
-  const { title, author, price, cover, pages, published, language, genre, rating, description } = req.body;
 
-  const book = new Book({
-    title,
-    author,
-    price,
-    cover,
-    pages,
-    published,
-    language,
-    genre,
-    rating,
-    description
-  });
+cloudinary.config({
+  cloud_name: 'dfxpnludz',
+  api_key: '918177626788132',
+  api_secret: 'ru9snk5wFZtzPlFm71kqg0r7g1U'
+});
 
-  book.save()
-    .then(savedBook => {
-      res.status(201).json(savedBook);
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Failed to create book' });
-    });
+
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads'); // Specify the destination folder for temporary storage
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Generate a unique filename
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.fieldname === 'coverPic' || file.fieldname === 'pdfVersion') {
+    cb(null, true); // Accept the file
+  } else {
+    cb(null, false); // Reject the file
+  }
 };
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+
+exports.createBook = async (req, res) => {
+  upload.fields([{ name: 'coverPic', maxCount: 1 }, { name: 'pdfVersion', maxCount: 1 }])(req, res, async function (err) {
+    if (err) {
+      // Handle the error
+      res.status(500).json({ success: false, message: 'Failed to upload files' });
+    } else {
+      const uploadCover = req.files.coverPic[0];
+      const uploadPdf = req.files.pdfVersion[0];
+
+      try {
+        // Upload cover image to Cloudinary
+        const coverResult = await cloudinary.uploader.upload(uploadCover.path, {
+          folder: 'images',
+        });
+
+        // Upload PDF version to Cloudinary
+        const pdfResult = await cloudinary.uploader.upload(uploadPdf.path, {
+          folder: 'pdfs',
+        });
+        console.log(coverResult.secure_url + " " + pdfResult.secure_url);
+
+        // Retrieve other form data
+        const { title, author, price, genre, description, discount, rating, language, pages, availableQuantity } = req.body;
+
+        // Create the book object with the form data and Cloudinary URLs
+        const newBook = new Book({
+          title,
+          author,
+          price,
+          genre,
+          availableQuantity,
+          pages,
+          language,
+          rating,
+          discount,
+          description,
+          cover: coverResult.secure_url,
+          PDFLink: pdfResult.secure_url,
+        });
+
+        // Save the new book to the database
+        await newBook.save();
+
+        // Return a success response
+        res.json({ success: true, message: 'Book created successfully' });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Failed to create book', error });
+      }
+    }
+  });
+};
+
+
+
 // Read all books
 exports.getAllBooks = (req, res) => {
   Book.find()
@@ -73,9 +136,10 @@ exports.getTopRatedBooks = (req, res) => {
     });
 };
 
-// Read favorite books by user
-exports.getFavoriteBooksByUser = (req, res) => {
-  const userId = req.params.userId;
+// Read favorites of a specific user
+exports.getUserFavorites = (req, res) => {
+  const userId = req.params.id;
+
   User.findById(userId).populate('favorites')
     .then(user => {
       if (user) {
@@ -85,10 +149,35 @@ exports.getFavoriteBooksByUser = (req, res) => {
       }
     })
     .catch(error => {
-      console.log(error);
-      res.status(500).json(error);
+      res.status(500).json({ error: 'Failed to retrieve favorites' });
     });
 };
+
+// Add to favorites
+exports.addToFavorites = (req, res) => { //book
+
+
+  const { bookId, userId } = req.body;
+  console.log("bookid", req.body);
+  User.findById(userId)
+    .then(user => {
+      const bid = new mongoose.mongo.ObjectId(bookId);
+      console.log("user", user, "bid", bid);
+      if (user.favorites.includes(bid)) {
+        user.favorites.pull(bid);
+        user.save();
+        return res.status(200).json({ message: 'Book pulled' });
+      }
+      user.favorites.push(bid);
+      user.save();
+      return res.status(200).json({ message: 'Book added' });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Failed to add to favorites' });
+    });
+};
+
 
 // Read purchased books by user
 exports.getPurchasedBooksByUser = (req, res) => {
@@ -149,7 +238,14 @@ exports.deleteBook = (req, res) => {
   Book.findByIdAndDelete(bookId)
     .then(deletedBook => {
       if (deletedBook) {
-        res.json({ message: 'Book deleted successfully' });
+        //delete the book from the users favorites
+        User.updateMany({}, { $pull: { favorites: bookId } })
+          .then(() => {
+            res.json({ message: 'Book deleted successfully' });
+          })
+          .catch(error => {
+            console.log(error);
+          });
       } else {
         res.status(404).json({ error: 'Book not found' });
       }
@@ -159,16 +255,47 @@ exports.deleteBook = (req, res) => {
     });
 };
 
-//search books
-exports.searchBooks = (req, res) => {
-  const query = req.query.q;
+// Define a route for searching books
 
-  Book.find({ title: { $regex: query, $options: 'i' } })
-    .then(books => {
-      res.json(books);
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Failed to retrieve books' });
+exports.searchBooks = async (req, res) => {
+  try {
+    const { query } = req.query;
+
+    // Perform the search query
+    const searchResults = await Book.find({
+      $or: [
+        { title: { $regex: query, $options: 'i' } },
+        { description: { $regex: query, $options: 'i' } },
+        { genre: { $regex: query, $options: 'i' } },
+        { author: { $regex: query, $options: 'i' } },
+      ],
     });
-}
 
+    res.json({ results: searchResults });
+  } catch (error) {
+    console.error('Error searching books:', error);
+    res.status(500).json({ error: 'Failed to search books' });
+  }
+};
+
+//search by provided filters
+exports.searchByFilters = async (req, res) => {
+  try {
+    const { title, author, genre, language  } = req.query;
+
+    // Perform the search query
+    const searchResults = await Book.find({
+      $or: [
+        { title: { $regex: title, $options: 'i' } },
+        { genre: { $regex: genre, $options: 'i' } },
+        { author: { $regex: author, $options: 'i' } },
+        { language: { $regex: language, $options: 'i' } },
+      ],
+    });
+
+    res.json({ results: searchResults });
+  } catch (error) {
+    console.error('Error searching books:', error);
+    res.status(500).json({ error: 'Failed to search books' });
+  }
+};
--- routes/books.js
+++ routes/books.js
@@ -2,15 +2,34 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/booksController');
 
-router.post('/books', bookController.createBook);
-router.get('/books', bookController.getAllBooks);
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Set the destination folder for uploaded files
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // Use the original filename
+    },
+  });
+  const upload = multer({ storage: storage });
+
+router.post('/books',  bookController.createBook);
+router.post('/bookxs', upload.fields([{ name: 'coverPic', maxCount: 1 }, { name: 'pdfVersion', maxCount: 1 }]), bookController.createBook);
+
+router.get('/books',bookController.getAllBooks);
 router.get('/books/latest', bookController.getLatestBooks);
 router.get('/books/top-selling', bookController.getTopSellingBooks);
+
+router.post('/books/favorites', bookController.addToFavorites);
+router.get('/books/favorite/:userId', bookController.getUserFavorites);
+
 router.get('/books/top-rated', bookController.getTopRatedBooks);
-router.get('/books/favorite/:userId', bookController.getFavoriteBooksByUser);
 router.get('/books/purchased/:userId', bookController.getPurchasedBooksByUser);
 router.get('/books/:id', bookController.getBookById);
-router.put('/books/:id', bookController.updateBook);
+router.put('/books',  bookController.updateBook);
+
 router.delete('/books/:id', bookController.deleteBook);
+router.get('/books/search', bookController.searchBooks);
 
 module.exports = router;
