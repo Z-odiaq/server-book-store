f4fd540ad75842b2099d4c1dab7764cde495a78e
Mohamed Ammar <mohamedammar10@live.fr>
2023-07-03 16:23:20 +0100

    added mailing support, coupon code generation

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -1,6 +1,114 @@
 const Order = require('../models/Order');
 const Book = require('../models/Book');
 const Coupon = require('../models/Coupon');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.MAILER_EMAIL_ID,
+    pass: process.env.MAILER_PASSWORD,
+  },
+});
+
+
+async function sendCouponCodeEmail(userEmail, coupon) {
+  try {
+    // Compose the email message
+    console.log(process.env.MAILER_PASSWORD);
+    const mailOptions = {
+      from: process.env.MAILER_EMAIL_ID,
+      to: userEmail,
+      subject: 'Coupon Code',
+      text: `Dear user, your coupon code is: ${coupon.Code}.`,
+      html: `<!DOCTYPE html>
+      <html>
+        <head>
+          <title>Special Offer</title>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+          <style>
+            .container {
+              max-width: 600px;
+              margin: 0 auto;
+              padding: 20px;
+              background-color: #f8f8f8;
+              border: 1px solid #e6e6e6;
+              border-radius: 4px;
+            }
+      
+            h1 {
+              color: #333;
+              font-size: 24px;
+              text-align: center;
+              margin-bottom: 20px;
+            }
+      
+            p {
+              color: #555;
+              font-size: 16px;
+              line-height: 1.5;
+              margin-bottom: 10px;
+            }
+      
+            .coupon {
+              background-color: #fff;
+              border: 1px solid #e6e6e6;
+              border-radius: 4px;
+              padding: 15px;
+              margin-bottom: 20px;
+            }
+      
+            .coupon p {
+              margin-bottom: 5px;
+            }
+      
+            .coupon-code {
+              font-size: 20px;
+              font-weight: bold;
+              color: #333;
+              margin-bottom: 10px;
+            }
+      
+            .coupon-details {
+              color: #777;
+              font-size: 14px;
+            }
+      
+            .footer {
+              text-align: center;
+              margin-top: 20px;
+              color: #888;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Special Offer!</h1>
+            <p>Dear valued customer,</p>
+            <p>As a token of our appreciation, we are pleased to offer you a special discount coupon:</p>
+            <div class="coupon">
+              <p class="coupon-code">Coupon Code: ${coupon.Code}</p>
+              <p class="coupon-details">Discount: ${coupon.percentage}% off your next purchase</p>
+              <p class="coupon-details">Expiration Date: ${coupon.Code.split[T]}</p>
+            </div>
+            <p>Visit our website or store to redeem this coupon and enjoy great savings on our products.</p>
+            <p>Thank you for being a loyal customer!</p>
+            <p class="footer">Best regards, <br>Your Company </p>
+          </div>
+        </body>
+      </html>`,
+
+    };
+
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
 
 // Create an order and update book sold
 exports.createOrder = async (req, res) => {
@@ -38,6 +146,8 @@ exports.createOrder = async (req, res) => {
       return res.status(400).json({ error: 'Invalid coupon code' });
     }
 
+
+
     const order = new Order({
       user: req.body.id,
       books: books,
@@ -62,11 +172,38 @@ exports.createOrder = async (req, res) => {
       await bookData.save();
     }
 
+
+    if (total > 100) {
+      //create coupon and send it by email
+      const coupon = new Coupon({
+        code: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+        discountPercentage: 10,
+        expiryDate: new Date().setDate(new Date().getDate() + 30),
+        user: req.body.id
+      });
+      const savedCoupon = await coupon.save().then((coupon) => {
+        sendCouponCodeEmail(req.body.email, coupon);
+      });
+    }
+    if (total > 200) {
+      //create coupon and send it by email
+      const coupon = new Coupon({
+        code: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+        discountPercentage: 15,
+        expiryDate: new Date().setDate(new Date().getDate() + 30),
+        user: req.body.id
+      });
+      const savedCoupon = await coupon.save().then((coupon) => {
+        sendCouponCodeEmail(req.body.email, coupon);
+      });
+    }
     res.status(201).json(savedOrder);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: 'Failed to create order' });
   }
+
+
 };
 
 // Read all orders
